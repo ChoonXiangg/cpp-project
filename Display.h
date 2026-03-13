3f786f3e1daba1cd4aaaae83a2a10748620b7638
ChoonXiangg <yeohcx192@gmail.com>
2026-03-13 13:38:40 +0000

    Add HexDisplay::PrintGrid to Display.h, declare a new overload that takes HexGrid instead of SquareGrid.

--- Display.h
+++ Display.h
@@ -3,12 +3,21 @@
 #include <vector>
 #include "SquareGrid.h"
 #include "SquareNode.h"
+#include "HexGrid.h"
+#include "HexNode.h"
 
 namespace Display {
     void EnableAnsiEscapes();
+
     void PrintGrid(const Grid& grid,
         GridNode* start, GridNode* target,
         GridNode* agent,
         const std::vector<GridNode*>& visited);
+
+    void PrintGrid(const HexGrid& grid,
+        HexNode* start, HexNode* target,
+        HexNode* agent,
+        const std::vector<HexNode*>& visited);
+
     void MoveCursorUp(int lines);
 }
